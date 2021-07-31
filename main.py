ad12ca23e6676c28f1f3d6538624a93920cd408a
jciterceros <jciterceros@yahoo.com>
2021-07-31 11:52:03 -0300

    Separando por tipo de moeda

--- main.py
+++ main.py
@@ -6,3 +6,20 @@ requisicao = requests.get(link)
 
 print(requisicao.json())
 
+""" Resultado
+{'USDBRL': {'code': 'USD', 'codein': 'BRL', 'name': 'Dólar Americano/Real Brasileiro', 'high': '5.228', 'low': '5.0758', 'varBid': '0.1305', 'pctChange': '2.57', 'bid': '5.2103', 'ask': '5.213', 'timestamp': '1627678795', 'create_date': '2021-07-30 21:00:01'}, 'EURBRL': {'code': 'EUR', 'codein': 'BRL', 'name': 'Euro/Real Brasileiro', 'high': '6.2013', 'low': '6.0331', 'varBid': '0.149', 'pctChange': '2.47', 'bid': '6.1859', 'ask': '6.1912', 'timestamp': '1627678797', 'create_date': '2021-07-30 21:00:01'}, 'BTCBRL': {'code': 'BTC', 'codein': 'BRL', 'name': 'Bitcoin/Real Brasileiro', 'high': '219.501', 'low': '200.567', 'varBid': '12604', 'pctChange': '6.24', 'bid': '214.55', 'ask': '214.6', 'timestamp': '1627742358', 'create_date': '2021-07-31 11:39:18'}}
+"""
+
+print()
+lista = []
+dados = requisicao.json()
+lista.append(dados)
+for cotacao in lista:
+    for k,v in cotacao.items():
+        print(k + " : "+str(v))
+
+""" Resultado
+USDBRL : {'code': 'USD', 'codein': 'BRL', 'name': 'Dólar Americano/Real Brasileiro', 'high': '5.228', 'low': '5.0758', 'varBid': '0.1305', 'pctChange': '2.57', 'bid': '5.2103', 'ask': '5.213', 'timestamp': '1627678795', 'create_date': '2021-07-30 21:00:01'}
+EURBRL : {'code': 'EUR', 'codein': 'BRL', 'name': 'Euro/Real Brasileiro', 'high': '6.2013', 'low': '6.0331', 'varBid': '0.149', 'pctChange': '2.47', 'bid': '6.1859', 'ask': '6.1912', 'timestamp': '1627678797', 'create_date': '2021-07-30 21:00:01'}
+BTCBRL : {'code': 'BTC', 'codein': 'BRL', 'name': 'Bitcoin/Real Brasileiro', 'high': '219.501', 'low': '200.567', 'varBid': '12604', 'pctChange': '6.24', 'bid': '214.55', 'ask': '214.6', 'timestamp': '1627742358', 'create_date': '2021-07-31 11:39:18'}
+"""
